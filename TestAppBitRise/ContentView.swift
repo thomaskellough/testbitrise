424a5af4de41cd85fba589c92f221c658c2330e1
thomas.kellough <thomas.kellough@brandtinfo.com>
2021-04-07 14:12:51 -0600

    init commit

---
+++ TestAppBitRise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestAppBitRise
+//
+//  Created by Thomas Kellough on 4/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
